28552f0a7312d7dedb2a8745deae94d5cf7c1a90
gonzonia <36857621+gonzonia@users.noreply.github.com>
2025-06-17 14:26:03 -0400

    Adding some more functions

--- select.py
+++ select.py
@@ -1,57 +1,77 @@
 import logging
 import json
 import socket
-
 from homeassistant.components.select import SelectEntity
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-def send_json(host, port, json_obj):
+def send_json(host, port, obj):
     try:
-        json_str = json.dumps(json_obj)
         with socket.create_connection((host, port), timeout=2) as sock:
-            sock.sendall(json_str.encode("utf-8"))
+            sock.sendall(json.dumps(obj).encode("utf-8"))
         return True
     except Exception as e:
         _LOGGER.error("Failed to send JSON for select: %s", e)
         return False
 
+def get_json(host, port, feature):
+    try:
+        with socket.create_connection((host, port), timeout=2) as sock:
+            sock.sendall(json.dumps({"type": "get", "feature": feature}).encode("utf-8"))
+            return json.loads(sock.recv(4096).decode("utf-8")).get("value")
+    except Exception as e:
+        _LOGGER.error("Failed to get JSON: %s", e)
+        return None
+
 class SonyHDMIOutSelect(SelectEntity):
     def __init__(self, host, port):
         self._host = host
         self._port = port
-        self._attr_name = "Sony AVR HDMI Output"
+        self._attr_name = "Sony AVR HDMI Out"
         self._options = ["A", "B", "AB", "OFF"]
         self._current = None
 
     @property
-    def options(self):
-        return self._options
-
+    def options(self): return self._options
     @property
-    def current_option(self):
-        return self._current
+    def current_option(self): return self._current
 
-    def select_option(self, option: str) -> None:
-        if option in self._options:
-            send_json(self._host, self._port, {"type": "set", "feature": "hdmi.out", "value": option})
-            self._current = option
+    def select_option(self, option):
+        send_json(self._host, self._port, {"type": "set", "feature": "hdmi.out", "value": option})
+        self._current = option
 
     def update(self):
-        # Poll current HDMI output
-        try:
-            with socket.create_connection((self._host, self._port), timeout=2) as sock:
-                sock.sendall(json.dumps({"type": "get", "feature": "hdmi.out"}).encode("utf-8"))
-                resp = sock.recv(4096).decode("utf-8")
-                data = json.loads(resp)
-                self._current = data.get("value")
-        except Exception as e:
-            _LOGGER.error("Failed to poll HDMI output: %s", e)
+        self._current = get_json(self._host, self._port, "hdmi.out")
 
-async def async_setup_platform(hass, config: ConfigType, async_add_entities, discovery_info: DiscoveryInfoType = None):
+class SonySoundFieldSelect(SelectEntity):
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        self._attr_name = "Sony AVR Sound Field"
+        self._options = [
+            "2ch", "direct", "afd", "multi", "hddcs", "dolby", "pl2", "pl2x", "pl2_music",
+            "pl2x_music", "neo6", "neo6_movie", "neo6_music", "jazz", "concert", "stadium",
+            "sports", "hall_a", "hall_b", "hall_c"
+        ]
+        self._current = None
+
+    @property
+    def options(self): return self._options
+    @property
+    def current_option(self): return self._current
+
+    def select_option(self, option):
+        send_json(self._host, self._port, {"type": "set", "feature": "audio.soundfield", "value": option})
+        self._current = option
+
+    def update(self):
+        self._current = get_json(self._host, self._port, "audio.soundfield")
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     host = config.get("host")
     port = config.get("port")
-    async_add_entities([SonyHDMIOutSelect(host, port)])
+    async_add_entities([
+        SonyHDMIOutSelect(host, port),
+        SonySoundFieldSelect(host, port)
+    ])
